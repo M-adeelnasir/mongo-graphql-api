78b02b908a8413bbe4a8a4ed7d1652749dce5ba1
unknown <adeelnasirkbw@gmail.com>
2022-11-12 18:19:16 -0800

    login servie created

--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -1,10 +1,41 @@
-import { UserModel } from '..//schema/user.schema';
-import { UserRegisterInput } from '..//schema/user.schema';
+import { Response } from 'express';
+import { UserLoginInput, UserModel } from '../schema/user.schema';
+import { UserRegisterInput } from '../schema/user.schema';
+import argon from 'argon2';
+import { jwtSign } from '../utils/jwt';
+import { ApolloError } from 'apollo-server';
+
 export class UserService {
   async createUser(input: UserRegisterInput) {
-    console.log(input);
-
     const user = await UserModel.create(input);
     return user;
+  }
+
+  async loginUser(input: UserLoginInput, res: Response) {
+    const { email, password } = input;
+
+    const user = await UserModel.find().findByEmail(email).lean();
+
+    if (!user) {
+      throw new ApolloError('Invalid credentials');
+    }
+
+    const isMatch = await argon.verify(user.password, password);
+
+    if (!isMatch) {
+      throw new ApolloError('Invalid credentials');
+    }
+
+    const token = await jwtSign({ email: user.email, _id: user.email });
+
+    res.cookie('accessToken', token, {
+      maxAge: 3.154e10, //1year
+      httpOnly: true,
+      domain: 'localhost',
+      path: '/',
+      sameSite: 'strict',
+      secure: process.env.NODE_ENV === 'production',
+    });
+    return token;
   }
 }
