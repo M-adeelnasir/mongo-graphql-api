3ce2c248234d44bd5519bdba551c721b4c257acf
unknown <adeelnasirkbw@gmail.com>
2022-11-12 16:34:15 -0800

    express and apollo server bootstraping

---
+++ src/index.ts
@@ -0,0 +1,47 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+import 'reflect-metadata';
+import express from 'express';
+import { buildSchema } from 'type-graphql';
+import { ApolloServer } from 'apollo-server-express';
+// import { ApolloServerPluginLandingPageProductionDefault } from 'apollo-server-core';
+// import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core';
+import mongo from './utils/mongo';
+import { resolvers } from './resolvers';
+
+const PORT = process.env.PORT;
+async function bootstrap() {
+  //   @ Build Schema
+  const schema = await buildSchema({
+    resolvers,
+  });
+
+  //   @ Initialize application
+  const app = express();
+
+  //create apollo server
+  const server = new ApolloServer({
+    schema,
+    context: (ctx) => {
+      return ctx;
+    },
+    // plugins: [
+    //   process.env.NODE_ENV === 'production'
+    //     ? ApolloServerPluginLandingPageProductionDefault()
+    //     : ApolloServerPluginLandingPageGraphQLPlayground(),
+    // ],
+  });
+
+  await server.start();
+
+  //applumiddleware
+  server.applyMiddleware({ app });
+
+  await mongo();
+  //   @ listen the server
+  app.listen(PORT, async () => {
+    console.log('Server is listing and up on port 1337');
+  });
+}
+
+bootstrap();
